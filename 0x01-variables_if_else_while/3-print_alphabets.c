c66b4d475efbd111bdc1ea5b32fd5656a53cf837
kagstaz <edwardkags@yahoo.co.uk>
2023-08-11 06:31:22 -0700

    the alphabet in lowercase, and then in uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the alphabet in lowercase, and then in uppercase'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 97;
+	int o = 65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+	}
+	while (o <= 90)
+	{
+		putchar(o);
+	}
+	putchar('\n');
+	return (0);
+}
